1f2445cba67c75841df0276275715a484ca8426e
Sebastian T F <sebastian.tf97@gmail.com>
2021-01-18 16:56:59 +0530

    use outputs to pass inputs

---
+++ scripts/create-some-files.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ls -lR
+
+mkdir some-files
+echo "file1" > some-files/file1.txt
+echo "file2" > some-files/file2.txt
+echo "file3" > some-files/file3.txt
---
+++ scripts/show-some-files.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd some-files
+ls -alR
